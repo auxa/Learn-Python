e23bdf55eb64750019be7da181b5faf0fe357c44
Joseph Fitzpatrick <jfitzpa1@tcd.ie>
2017-10-11 16:55:32 +0100

    Updates

--- test.py
+++ test.py
@@ -2,6 +2,38 @@ import wget
 import gzip
 
 
+def removeEmpty(dataSet):
+    dataSet.remove("")
+    return dataSet
+
+def searchForQuantity( wVal, hVal, dataSet):
+    wIndex = columns.index('device_width')
+    hIndex =columns.index('device_height')
+    frequency =0
+    for data in dataSet:
+        if data[wIndex] == str(wVal) and data[hIndex] == str(hVal):
+            frequency +=1
+    return frequency
+
+def splitIt(dataSet):
+    newData = []
+    for data in dataSet:
+        datum = data.split(',')
+        newData.append(datum)
+
+    return newData
+
+def sumDollars(dataSet):
+    dollars=0;
+    index = columns.index('spend')
+    for data in dataSet:
+        dollars = dollars +  float(data[index])
+    return dollars
+
+
+
+
+
 file_url = "https://s3.amazonaws.com/swrve-public/full_stack_programming_test/test_data.csv.gz"
 
 #file_name = wget.download(file_url)
@@ -15,17 +47,21 @@ strFile = file_content.decode("utf-8")
 #file123 = open("test.txt", "w")
 #file123.write(strFile)
 
-arrData = strFile.split("\n")
+arrData = strFile.split("\n") # Splits file into rows
+arrData= removeEmpty(arrData)  #removes empty rows
 
-print (arrData)
+arrData = splitIt(arrData)
 
-data= arrData[1:]
+columns = arrData[0]    #denotes this as the row with the coulmn names
+data= arrData[1:]   #rest of data without column names
 
-print(data)
-columnsStr = arrData[1]
-columns = columnsStr.split(',')
-print(columns)
 
+
+res = searchForQuantity(640,960, data)
+spend = sumDollars(data)
+print(spend)
+print(res)
 index1 = columns[3]
 
-print(str(index1))
+print(columns)
+print(len(data))
